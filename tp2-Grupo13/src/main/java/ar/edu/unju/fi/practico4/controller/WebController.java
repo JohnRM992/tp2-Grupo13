03fa32be03fac39795ec2f29ba4f36563ee5344d
JonathanRM <JonathanRM@DESKTOP-GC7DFCG>
2023-05-22 09:55:21 -0300

    Integración bootstrap,responsividad en index,consejos

--- tp2-Grupo13/src/main/java/ar/edu/unju/fi/practico4/controller/WebController.java
+++ tp2-Grupo13/src/main/java/ar/edu/unju/fi/practico4/controller/WebController.java
@@ -3,46 +3,40 @@ package ar.edu.unju.fi.practico4.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 
-import ar.edu.unju.fi.practico4.model.FormProducto;
+//import ar.edu.unju.fi.practico4.model.FormProducto;
 
 @Controller
 public class WebController {
-		@GetMapping("/index")
-		public String getIndexPage(Model model) {
-				return "index";
-			}
-		
-	 
-		@GetMapping("/productos")
-		public String getProductosPage(Model model) {
-				return "productos";
-			}
-		
-		
-	
-		
-		
-		@GetMapping("/consejos")
-		public String getConsejosPage(Model model) {
-				return "consejos";
-			}
-		
-		@GetMapping("/servicios")
-		public String getServiciosPage(Model model) {
-				return "servicios";
-			}
-		
-		@GetMapping("/sucursales")
-		public String getSucursalesPage(Model model) {
-				return "sucursales";
-			}
-	
-		@GetMapping("/contacto")
-		public String getContactoPage(Model model) {
-				return "contacto";
-			}
-		
+	@GetMapping("/index")
+	public String getIndexPage(Model model) {
+		return "index";
+	}
+
+	@GetMapping("/productos")
+	public String getProductosPage(Model model) {
+		return "productos";
+	}
+
+	@GetMapping("/consejos")
+	public String getConsejosPage(Model model) {
+		return "consejos";
+	}
+
+	@GetMapping("/servicios")
+	public String getServiciosPage(Model model) {
+		return "servicios";
+	}
+
+	@GetMapping("/sucursales")
+	public String getSucursalesPage(Model model) {
+		return "sucursales";
+	}
+
+	@GetMapping("/contacto")
+	public String getContactoPage(Model model) {
+		return "contacto";
+	}
 
 }
