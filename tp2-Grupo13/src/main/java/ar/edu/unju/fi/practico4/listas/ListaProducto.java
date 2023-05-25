067a82687a183da9a197ed08248721f281de28af
JonathanRM <JonathanRM@DESKTOP-GC7DFCG>
2023-05-25 18:15:28 -0300

    Limpieza codigo CSS, cambios estilo en HTMLs

---
+++ tp2-Grupo13/src/main/java/ar/edu/unju/fi/practico4/listas/ListaProducto.java
@@ -0,0 +1,20 @@
+//package ar.edu.unju.fi.practico4.listas;
+////package ar.edu.unju.fi.listas;
+//
+//import java.util.List;
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//
+//import ar.edu.unju.fi.practico4.model.Producto;
+//public class ListaProducto {
+//		private List<Producto> productos;
+//		
+//		
+//		
+//		public ListaProducto() {
+//			productos = new ArrayList<Producto>();
+//			productos.add(new Producto(Producto.get  ,"Tucuman 133", LocalDate.of(2020,10,10),"ladau223@gmail.com", "13546");
+//		}
+//		
+//}
+//
